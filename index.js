861acafa1225ef6b03b820c8d156c0bbf24096ec
PiyushSharma32 <72690317+piyushpoudyal@users.noreply.github.com>
2021-11-29 04:49:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var readlinesync= require("readline-sync");
+var score=0;
+
+function play(question,answer){
+var userQuestion= readlinesync.question(question);
+
+if (userQuestion ==answer){
+  console.log("Correct");
+  score++;
+  console.log("Socre: ",score)
+}else{
+  console.log("Nops try Again");
+  score--;
+  console.log("Socre: ",score)
+}
+}
+
+play("My Name","Piyush";
+play("Age","19");
+play("from","sikkim");
+play("WOWOW","heheh");
